c4061c2d3603115ae2480769b5c5e8ef44318a2d
Ricky Pai <rickyp999+github@gmail.com>
2020-11-08 15:24:15 -0800

    fix go mod config and init grpc server

--- api/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/rickypai/mmtk/api
-
-go 1.15
--- api/main.go
+++ api/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/rickypai/mmtk/protobuf/api"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	api.UnimplementedAPIServer
+}
+
+func (s *server) GetImage(ctx context.Context, in *api.GetImageRequest) (*api.GetImageResponse, error) {
+	return &api.GetImageResponse{}, nil
+}
+
 func main() {
-	println("lol")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	api.RegisterAPIServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
