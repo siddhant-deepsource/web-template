4a02a5d2ea3fdbdd906969a44f6b34641302bd59
Ricky Pai <rickyp999+github@gmail.com>
2020-12-01 22:04:47 -0800

    simplify

--- web/pages/phone/[id].tsx
+++ web/pages/phone/[id].tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Error, StatusCode } from 'grpc-web';
+import { GetServerSidePropsContext } from 'next';
 import Container from '../../components/container';
 // import SEO from '../../components/seo';
 // import NotFoundPage from '../../pages/404';
@@ -7,10 +8,10 @@ import { GetPhoneRequest, GetPhoneResponse } from '../../protobuf/api/api_pb';
 import { Phone } from '../../protobuf/phone/phone_pb';
 import Client from '../../clients/node_client';
 
-export const getServerSideProps = async (context) => {
-  const { id } = context.params;
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const id = parseInt(context.params.id, 10);
 
-  const data: PhonePageState = {
+  const props: PhonePageProp = {
     id,
     phone: null,
     errorCode: null,
@@ -28,89 +29,76 @@ export const getServerSideProps = async (context) => {
   // await (p);
   await p.then(
     (response: GetPhoneResponse) => {
-      data.phone = response.getPhone().toObject();
+      props.phone = response.getPhone().toObject();
     },
     (e: Error) => {
-      data.errorCode = e.code;
+      props.errorCode = e.code;
     },
   ).catch(
     () => {
-      data.errorCode = StatusCode.UNKNOWN;
+      props.errorCode = StatusCode.UNKNOWN;
     },
   );
 
   return {
-    props: data,
+    props,
   };
 };
 
-interface PhonePageState {
+interface PhonePageProp {
   id: number;
   phone?: Phone.AsObject;
   errorCode?: number;
 }
 
-export default class PhonePage extends React.Component<
-  PhonePageState,
-  PhonePageState
-> {
-  constructor(props: PhonePageProps) {
-    super(props);
-
-    this.state = {
-      id: props.id,
-      phone: props.phone,
-      errorCode: props.errorCode,
-    };
+const PhonePage = (props: PhonePageProp): JSX.Element => {
+  if (props.phone) {
+    return (
+        <Container defKey="1">
+          {/* <SEO title={phone.getName()} /> */}
+          <h1>{props.phone.name}</h1>
+        </Container>
+    );
   }
 
-  render() {
-    if (this.state.phone) {
-      return (
-        <Container defKey="1">
-          {/* <SEO title={this.state.phone.getName()} /> */}
-          <h1>{this.state.phone.name}</h1>
-        </Container>
-      );
-    }
-
-    if (this.state.errorCode === StatusCode.NOT_FOUND) {
-      return (
+  if (props.errorCode === StatusCode.NOT_FOUND) {
+    return (
         // <NotFoundPage
         //   code={404}
         //   title="Phone not found"
-        //   message={`Phone with id=${this.state.id} not found.`}
+        //   message={`Phone with id=${id} not found.`}
         // />
         <Container defKey="1">
           <h1>404</h1>
         </Container>
-      );
-    }
+    );
+  }
 
-    if (this.state.errorCode === StatusCode.DEADLINE_EXCEEDED) {
-      return (
+  if (props.errorCode === StatusCode.DEADLINE_EXCEEDED) {
+    return (
         // <NotFoundPage
         //   code={503}
         //   title="Timeout"
         //   message={
-        //     `Waited too long while fetching phone with id=${this.state.id}`
+        //     `Waited too long while fetching phone with id=${id}`
         //   }
         // />
         <Container defKey="1">
           <h1>503</h1>
         </Container>
-      );
-    }
+    );
+  }
 
-    return (
+  return (
       // <NotFoundPage
       //   code={500}
       //   title="Internal server error"
-      //   message={`Error fetching phone with id=${this.state.id}`}
+      //   message={`Error fetching phone with id=${id}`}
       // />
       <Container defKey="1">
         <h1>500</h1>
       </Container>
-    );
-  }
-}
+  );
+};
+
+export default PhonePage;
