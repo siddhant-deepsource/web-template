6d510e291faf653d5445052b5c39c4cd01cc7605
Ricky Pai <rickyp999+github@gmail.com>
2020-12-01 22:37:18 -0800

    404

---
+++ web/pages/404.tsx
@@ -0,0 +1,9 @@
+import Container from '../components/container';
+
+const Custom404 = (): JSX.Element => (
+  <Container defKey={[]}>
+    <h1>404 - Page Not Found</h1>
+  </Container>
+);
+
+export default Custom404;
